d6e39b8746fc2f43301ba2859d811bd6239a2ece
amani817 <amaniamare25@gmail.com>
2025-11-08 20:07:51 +0100

    Update notation.js

--- notation.js
+++ notation.js
@@ -1,6 +1,14 @@
 // notation.js
 function mean(scores) {
-    const total = scores.reduce((acc, score) => acc + score, 0);
-    return total / scores.length;
+    const sum = scores.reduce((acc, score) => acc + score, 0);
+    return sum / scores.length;
 }
-module.exports = mean;
+function generateRandomScores(numScores, min = 0, max = 100) {
+    const scores = [];
+    for (let i = 0; i < numScores; i++) {
+        const score = Math.floor(Math.random() * (max - min + 1)) + min;
+        scores.push(score);
+    }
+    return scores;
+}
+module.exports = { mean, generateRandomScores }; 
