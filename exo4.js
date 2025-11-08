252015f833de8e7350ceacb782e08594e70f80f0
amani817 <amaniamare25@gmail.com>
2025-11-08 20:08:37 +0100

    Update exo4.js

--- exo4.js
+++ exo4.js
@@ -2,6 +2,7 @@
 // exo4.js (modified)
 const fs = require('fs');
 
+
 // Get the filename and text from the command line arguments
 const filename = process.argv[2]; // The first argument is the filename
 const text = process.argv[3]; // The second argument is the text to save
@@ -14,7 +15,7 @@ fs.writeFile(filename, text, (err) => {
     }
     console.log('The file has been saved!');
 
-    // Read and display the contents of the created file
+    // Read the contents of the created file
     fs.readFile(filename, 'utf8', (err, data) => {
         if (err) {
             console.error('Error while reading the file:', err);
